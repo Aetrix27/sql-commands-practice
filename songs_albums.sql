f9c0e3d217ff303941733d348620862aa41dd40a
Aetrix27 <davedogg323@gmail.com>
2021-01-27 21:29:22 -0800

    Different tables display

--- songs_albums.sql
+++ songs_albums.sql
@@ -14,10 +14,30 @@ CREATE TABLE Albums (
     year_published INTEGER NOT NULL
 );
 
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    album_id INTEGER NOT NULL,
+    name VARCHAR(70) NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES Albums(id)
+);
+
 /* 
  * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
+
+ * 
  */
- 
+
+INSERT INTO Songs
+    (name, album_id)
+VALUES
+    ('Come Together', 2),
+    ('Too Late', 5),
+    ('Life in the Fast Lane', 3),
+    ('Darlington County', 4),
+    ('Darlington County', 1)
+;
+
+
 INSERT INTO Albums
     (name, artist, year_published)
 VALUES
@@ -25,19 +45,24 @@ VALUES
     ('Abbey Road', 'The Beatles', 1969),
     ('Hotel California', 'Eagles', 1976),
     ('Born in the U.S.A.', 'Bruce Springsteen', 1984),
-    ('California', 'Blink-182', 2016)
+    ('After Hours', 'The Weeknd', 2020)
 ;
 
 
 /* Queries */
 
--- SELECT * FROM Songs;
+SELECT * FROM Songs;
 
 SELECT * FROM Albums;
 
 /* 
  * TODO: Write a table join query to construct a table of Song Name : Album Name
+SELECT Songs.name FROM Songs
+JOIN Albums ON Albums.id = Songs.album_id
+
  */
+
+
 
 /*
  * TODO: Find all albums published between 1970 and 1980.
